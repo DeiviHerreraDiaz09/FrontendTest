39e5cd598e27e62c05f008f6b65574f18d92d1d4
DeiviHerreraDiaz09 <dherrera@grupoasd.com>
2024-05-18 12:07:46 -0500

    FEAT: Success Card Component

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-// import QrCode from './assets/QrCode/QrCode.jsx' // COMPONENT QR
+import QrCode from './assets/QrCode/QrCode.jsx' // COMPONENT QR
 import CardComponent from './assets/CardComponent/CardComponent.jsx' // COMPONENT CARD
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <QrCode /> QR CODE COMPONENT */}
+    {/* <QrCode /> */}
     <CardComponent></CardComponent>
   </React.StrictMode>,
 )
