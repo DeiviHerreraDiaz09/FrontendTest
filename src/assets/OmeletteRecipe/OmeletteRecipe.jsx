20d017f92e44ec267f46dd8ad99c7d76f29d5a1f
DeiviHerrera09 <dherrera@grupoasd.com>
2024-05-21 06:54:21 -0500

    FEAT: add recipe component

---
+++ src/assets/OmeletteRecipe/OmeletteRecipe.jsx
@@ -0,0 +1,16 @@
+"use client";
+import React from 'react';
+import './OmeletteRecipe.css';
+import PropTypes from 'prop-types';
+
+const OmeletteRecipe = ({}) => {
+	return (
+		<div className='omeletterecipe'>
+ 			OmeletteRecipe works!
+ 		</div>
+	);
+};
+
+OmeletteRecipe.propTypes = {};
+
+export default OmeletteRecipe;
---
+++ src/assets/OmeletteRecipe/index.js
@@ -0,0 +1,1 @@
+export { default as OmeletteRecipe } from './OmeletteRecipe';
