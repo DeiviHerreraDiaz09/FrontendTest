81302a7f35ba12b4a87956c34efbaa42b746d380
DeiviHerreraDiaz09 <herreradiazdeivistiven2004@gmail.com>
2024-05-30 16:02:04 -0500

    FEAT: Init component countries

---
+++ src/assets/CountriesComponent/CountriesComponent.jsx
@@ -0,0 +1,16 @@
+"use client";
+import React from 'react';
+import './CountriesComponent.css';
+import PropTypes from 'prop-types';
+
+const CountriesComponent = ({}) => {
+	return (
+		<div className='countriescomponent'>
+ 			CountriesComponent works!
+ 		</div>
+	);
+};
+
+CountriesComponent.propTypes = {};
+
+export default CountriesComponent;
