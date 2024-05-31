a46a241650b99c9e21b0e08acd5a5c6eac481468
DeiviHerreraDiaz09 <herreradiazdeivistiven2004@gmail.com>
2024-05-31 16:02:18 -0500

    Component countries v2

--- src/assets/CountriesComponent/CountriesComponent.jsx
+++ src/assets/CountriesComponent/CountriesComponent.jsx
@@ -1,14 +1,36 @@
 "use client";
-import React from 'react';
-import './CountriesComponent.css';
-import PropTypes from 'prop-types';
+import React from "react";
+import "./CountriesComponent.css";
+import PropTypes from "prop-types";
 
 const CountriesComponent = ({}) => {
-	return (
-		<div className='countriescomponent'>
- 			CountriesComponent works!
- 		</div>
-	);
+  return (
+    <div className="countriescomponent">
+      <header>
+        <h1>Where in the world?</h1>
+        <div className="modeScreen">
+          <div className="moonMood"></div>
+          <h3>Dark Mode</h3>
+        </div>
+      </header>
+
+      <div className="filters">
+        <div className="search">
+          <div className="loupeImg"></div>
+          <input type="text" placeholder="Search for a country..." />
+        </div>
+
+        <select name="regions" className="regions">
+          <option selected disabled value="0">Filter By Region</option>
+          <option value="Africa">Africa</option>
+          <option value="America">America</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
+      </div>
+    </div>
+  );
 };
 
 CountriesComponent.propTypes = {};
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import QrCode from "./assets/QrCode/QrCode.jsx"; // COMPONENT QR
 import CardComponent from "./assets/CardComponent/CardComponent.jsx"; // COMPONENT CARD
 import OmeletteRecipe from "./assets/OmeletteRecipe/OmeletteRecipe.jsx"; // COMPONENT RECIPE - OMELETTE
 import ExpensesComponent from "./assets/ExpensesComponent/ExpensesComponent.jsx"; // COMPONENT EXPENSES
+import CountriesComponent from "./assets/CountriesComponent/CountriesComponent.jsx" // COMPONENT COUNTRIES
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <QrCode /> */}
     {/* <CardComponent/> */}
     {/* <OmeletteRecipe/> */}
-    <ExpensesComponent/>
+    {/* <ExpensesComponent/> */}
+    <CountriesComponent/>
   </React.StrictMode>
 );
